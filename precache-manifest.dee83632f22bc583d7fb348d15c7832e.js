5838178017a8fecba7932585b63c689eace005e9
Makoto <makotorasahi@gmail.com>
2020-09-17 11:46:37 -0700

    Updates

---
+++ precache-manifest.dee83632f22bc583d7fb348d15c7832e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6574a8f59f2cbbec51aeeb249b6d930f",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "bf449953a50c9046a22e",
+    "url": "/reactapp/static/js/2.703a1018.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.703a1018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5b94eb10240a73decb",
+    "url": "/reactapp/static/js/main.f19c5b2a.chunk.js"
+  },
+  {
+    "revision": "e219a2030a13ac34f0be",
+    "url": "/reactapp/static/js/runtime-main.f935dd26.js"
+  }
+]);
